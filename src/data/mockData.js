2521c2ddfab8205afdd7e647e498d22912465c4b
saeid <saeid.rohparvar@gmail.com>
2023-04-26 09:52:58 +0330

    start

---
+++ src/data/mockData.js
@@ -0,0 +1,82 @@
+export const mockDataTeam = [
+    {
+        id: 1,
+        name: 'Saeid Rouhparvar',
+        email: 'saeid.rohparvar@gmail.com',
+        age: 34,
+        phone: '(+98)9141245860',
+        access: 'admin'
+    },
+    {
+        id: 2,
+        name: 'Vahid Rouhparvar',
+        email: 'vahid.rohparvar@gmail.com',
+        age: 34,
+        phone: '(+98)9143232861',
+        access: 'manager'
+    },
+    {
+        id: 3,
+        name: 'Farid Rouhparvar',
+        email: 'farid.rohparvar@gmail.com',
+        age: 34,
+        phone: '(+98)9149171057',
+        access: 'admin'
+    },
+    {
+        id: 4,
+        name: 'Saeid Rouhparvar',
+        email: 'saeid.rohparvar@gmail.com',
+        age: 34,
+        phone: '(+98)9141245860',
+        access: 'admin'
+    },
+    {
+        id: 5,
+        name: 'Saeid Rouhparvar',
+        email: 'saeid.rohparvar@gmail.com',
+        age: 34,
+        phone: '(+98)9141245860',
+        access: 'admin'
+    },
+    {
+        id: 6,
+        name: 'Saeid Rouhparvar',
+        email: 'saeid.rohparvar@gmail.com',
+        age: 34,
+        phone: '(+98)9141245860',
+        access: 'admin'
+    },
+    {
+        id: 7,
+        name: 'Saeid Rouhparvar',
+        email: 'saeid.rohparvar@gmail.com',
+        age: 34,
+        phone: '(+98)9141245860',
+        access: 'admin'
+    },
+    {
+        id: 8,
+        name: 'Saeid Rouhparvar',
+        email: 'saeid.rohparvar@gmail.com',
+        age: 34,
+        phone: '(+98)9141245860',
+        access: 'admin'
+    },
+    {
+        id: 9,
+        name: 'Saeid Rouhparvar',
+        email: 'saeid.rohparvar@gmail.com',
+        age: 34,
+        phone: '(+98)9141245860',
+        access: 'admin'
+    },
+    {
+        id: 10,
+        name: 'Saeid Rouhparvar',
+        email: 'saeid.rohparvar@gmail.com',
+        age: 34,
+        phone: '(+98)9141245860',
+        access: 'admin'
+    },
+]
